6eb7a0d9637d2c60ff6f0f4836138f022f778150
Magnus <magnustheodorengh@gmail.com>
2026-09-05 21:58:34 +0200

    Maybe cilium fix

--- talos/scripts/control-planes.sh
+++ talos/scripts/control-planes.sh
@@ -3,12 +3,21 @@
 # Get the node's internal IP
 NODE_IP="192.168.0.188"
 
+
+talosctl machineconfig patch controlplane.yaml \
+  --patch @nodes/cp_192.168.0.188.yaml \
+  --patch @patches/remove_cni.yaml \
+  --patch @patches/wakeonlan.yaml \
+  --patch @patches/workload-on-controlplane.yaml \
+  --output rendered/cp_192.168.0.188.yaml
+
+# Apply with --insecure (temporary, until talosctl TLS is fixed)
+talosctl apply-config --insecure --nodes 192.168.0.188 --file rendered/cp_192.168.0.188.yaml
+
 helm repo add cilium https://helm.cilium.io/
 helm repo update
 
-helm template \
-    cilium \
-    cilium/cilium \
+helm install cilium cilium/cilium \
     --version 1.18.0 \
     --namespace kube-system \
     --set ipam.mode=kubernetes \
@@ -18,25 +27,4 @@ helm template \
     --set cgroup.autoMount.enabled=false \
     --set cgroup.hostRoot=/sys/fs/cgroup \
     --set k8sServiceHost=localhost \
-    --set k8sServicePort=7445 > cilium.yaml
-
-{
-  cat <<'EOF'
-apiVersion: v1alpha1
-kind: KubeInlineManifestConfig
-name: cilium
-manifest: |
-EOF
-  sed 's/^/  /' cilium.yaml
-} > patches/cilium-patch.yaml
-
-talosctl machineconfig patch controlplane.yaml \
-  --patch @nodes/cp_192.168.0.188.yaml \
-  --patch @patches/remove_cni.yaml \
-  --patch @patches/cilium-patch.yaml \
-  --patch @patches/wakeonlan.yaml \
-  --patch @patches/workload-on-controlplane.yaml \
-  --output rendered/cp_192.168.0.188.yaml
-
-# Apply with --insecure (temporary, until talosctl TLS is fixed)
-talosctl apply-config --insecure --nodes 192.168.0.188 --file rendered/cp_192.168.0.188.yaml
+    --set k8sServicePort=7445
